d0b35b0c97d56052307c8a29d8e8cd3b0b2a012e
steve911-prog <bwlad0911@gmail.com>
2022-12-18 21:27:00 +0100

    Portfolio making

---
+++ travel website/script/script.js
@@ -0,0 +1,10 @@
+let navbar = doscument.querySelector(".navbar"); // .header
+
+document.querySelector("#menu-btn").addEventListener("onclick", () => {
+  //navbar.classList.toggle("active");
+  navbar.style.display = "none";
+  console.log("IWORK");
+});
+
+//7:03
+//18:00
